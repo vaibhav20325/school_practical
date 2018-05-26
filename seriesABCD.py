38444df53195c2d548265a3345cc0b6a51432d74
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2018-05-26 18:26:33 +0530

    Update seriesABCD.py

--- seriesABCD.py
+++ seriesABCD.py
@@ -4,6 +4,6 @@ n=int(input("Enter number of rows: "))
 for i in range(0,n,1):
     cnt=cnt + i
     print((n-i-1)*" ", end="")
-    for j in range(0+cnt,i+1+cnt,1):
-        print(chr(j+ord("A")), end=" ")
+    for j in range(cnt,i+1+cnt,1):
+        print(chr(j%26+ord("A")), end=" ")
     print()
