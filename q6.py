cf2564ebff077a8d12d69f4dbea2473a8f5949c0
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2018-05-28 16:34:43 +0530

    Update and rename Q6.py to q6.py

--- q6.py
+++ q6.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 10 12:45:41 2018
-
-@author: amity
-"""
 a=int(input("Enter First Number "))
 b=int(input("Enter Second Number "))
 c=str(input("Enter Operator(+ - * /) "))
