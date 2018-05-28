d378bf9ffca1a89d53111d085efd0f76624e8c2c
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2018-05-28 16:34:02 +0530

    Update and rename Q5.py to q5.py

---
+++ q5.py
@@ -0,0 +1,18 @@
+m=int(input("Enter the amount of money "))
+a=int(m/1000)
+x=m%1000
+b=int(x/500)
+x=m%500
+c=int(x/100)
+x=m%100
+d=int(x/50)
+x=m%50
+e=int(x/10)
+x=m%10
+f=int(x/5)
+x=m%5
+g=int(x/2)
+x=m%2
+h=x
+print ("You have :",a,"Rs 1000 notes",b,"Rs 500 notes",c,"Rs 100 notes",d,"Rs 50 notes",e,"Rs 10 notes",f,"Rs 5 notes",g,"Rs 2 notes"
+,h,"Rs 1 notes")
