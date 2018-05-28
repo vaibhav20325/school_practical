8c7bef129cab6de8c8101a5576ab5f88fef630a5
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2018-05-28 16:35:17 +0530

    Update q7.py

--- q7.py
+++ q7.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 10 12:55:08 2018
-
-@author: amity
-"""
-
 h=int(input("Enter number of heads "))
 l=int(input("Enter number of legs "))
 a=int((l-2*h)/2)
