88960dee1a6cd2882367a5c7fb9ad16ee0420b93
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2018-05-28 16:49:08 +0530

    Add files via upload

---
+++ q14.py
@@ -0,0 +1,12 @@
+n=int(input("Enter the number:"))
+a=n
+rev=0
+while a>0:
+    d=a%10
+    a=a//10
+    rev=rev*10+d
+print (rev)
+if rev==n:
+    print ("The nummber is a palindrome")
+else:
+    print ("The number is not a palindrome")
