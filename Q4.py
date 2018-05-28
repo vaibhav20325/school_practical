4bf1be965e5a50aa8d6a6190daebcce94fbb16fd
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2018-05-28 16:32:35 +0530

    Update Q4.py

--- Q4.py
+++ Q4.py
@@ -1,13 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May  4 13:26:13 2018
-
-@author: amity
-"""
-
 name=input("Enter your Name ")
 age=int(input("Enter your age "))
 grade=input("Enter your Class ")
 n=age-1
-print ("Welcome ", name, "\nI am ",age, "years old and studying in class "
-       ,grade, "\nLast year I was ",n,"years old")
+print ("Welcome ", name, "\nI am ",age, "years old and studying in class ",grade, "\nLast year I was ",n,"years old")
