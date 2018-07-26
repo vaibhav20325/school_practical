2b9b57cfa59951bef213d1ad758907fafa965e02
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2018-07-26 13:26:27 +0530

    Create BaseConverter.py

---
+++ BaseConverter.py
@@ -0,0 +1,27 @@
+n=int(input("Enter number in Decimal: "))
+Dtype=str(input("Enter B for Binary, O for Octal, H for Hexadecimal: "))
+a=''
+d=0
+if n==0:
+    a='0'
+else:
+    if Dtype=='B':
+        while n!=0:
+            d=n%2
+            n=n//2
+            a+=str(d)
+    elif Dtype=='O':
+        while n!=0:
+            d=n%8
+            n=n//8
+            a+=str(d)
+    elif Dtype=='H':
+        while n!=0:
+            d=n%16
+            n=n//16
+            if d>9:
+                d=chr(ord('A')+d-10)
+            a+=str(d)
+    else:
+        print("Error")
+print(a[::-1])
