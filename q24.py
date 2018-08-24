83aee0c0563b0b4157a96eaf5e230e3a7ef240b4
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2018-08-24 13:48:46 +0530

    Update q24.py

--- q24.py
+++ q24.py
@@ -6,14 +6,19 @@ except:
     b=13
 c=''
 
-if a.upper()=='E':
-    for i in range (0,len(s)):
-        if s[i]!=' ':
-            d=ord(s[i])+b
-            if d>ord('Z'):
-                d=(d-ord('Z'))%26 +ord('A')-1
-            c+=chr(d)
-        else:
-            c+=' '
 
+if a.upper()=='D':
+    b=-1*b
+for i in range (0,len(s)):
+    if s[i]!=' ':
+        d=ord(s[i])+b
+        if d>ord('Z'):
+            d=(d-ord('Z'))%26 +ord('A')-1
+        elif d<ord('A'):
+            d=(d-ord('A'))%26 +ord('Z')-1
+        c+=chr(d)
+    else:
+        c+=' '
+
+    
 print(c)
