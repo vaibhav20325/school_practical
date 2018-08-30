fa8f13af7c0c56e5a557b57990d120b0e81a77f2
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2018-08-30 13:28:07 +0530

    Update q24.py

--- q24.py
+++ q24.py
@@ -1,11 +1,10 @@
 s=input("Enter String ").upper()
 a=input("Enter 'E' to encrypt, 'D' to decrypt ")
 try:
-    b=int(input("Enter number of rotation"))
+    b=int(input("Enter number of rotation "))
 except:
     b=13
 c=''
-
 
 if a.upper()=='D':
     b=-1*b
@@ -15,10 +14,10 @@ for i in range (0,len(s)):
         if d>ord('Z'):
             d=(d-ord('Z'))%26 +ord('A')-1
         elif d<ord('A'):
-            d=(d-ord('A'))%26 +ord('Z')-1
+            d=-1*((-d+ord('A'))%26) +ord('Z')+1
         c+=chr(d)
     else:
         c+=' '
 
-    
 print(c)
+
