06130a0ba02ce789b559e75707122d405b00f89b
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2019-01-24 21:20:33 +0530

    Create Menu.py

---
+++ Menu.py
@@ -0,0 +1,10 @@
+#MENU
+game=0
+print('Welcome, Which Game do you want to play?')
+while game not in [1,2,3]:
+    game=input('Choose (1) for Stack1e, (2) for TheekThaakToe, (3) for MineSweeper: ')
+if game==1:
+  
+if game==2:
+    
+if game==3:
