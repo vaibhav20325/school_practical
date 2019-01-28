9c593fcb6371b13708b943b01883830cc3ecd25d
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2019-01-28 16:03:41 +0530

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -2,9 +2,11 @@
 game=0
 print('Welcome, Which Game do you want to play?')
 while game not in [1,2,3]:
-    game=input('Choose (1) for Stack1e, (2) for TheekThaakToe, (3) for M\'Sweeper: ')
+    game=int(input('Choose (1) for Stack1e, (2) for TheekThaakToe, (3) for M\'Sweeper: '))
+
 if game==1:
-  
+    import main
 if game==2:
-    
+    import ttt
 if game==3:
+    
