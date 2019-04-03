f79c090e26c3ae3097be86f06d9458814e60b1e9
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2019-04-03 10:36:14 +0530

    Update q14 3.py

--- q14 3.py
+++ q14 3.py
@@ -1,18 +1,23 @@
 def sortl(l):
     return(l[2])
-
+d={}
 datalist=[]
 f=open('data.txt','r')
-
 for lines in f.readlines():
     lines=lines.strip()
-    l=lines.split('\t')
-    l=tuple(l)
+    l=tuple(lines.split('\t'))
     datalist.append(l)
-print(datalist)
 f.close()
 datalist.sort(key=sortl)
 for i in datalist:
     print(i)
-    
- 
+print('People with experience less than 3 yrs are:')   
+for i in datalist:
+    if int(i[2])<3:
+        print(i[0])
+    if i[3] not in d:
+        d[i[3]]=0
+        d[i[3]]+=1
+print('Number of people acc to dept:')
+for i in d:
+    print(i,':',d[i])
