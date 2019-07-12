6bf7a3f7800b0111bad2a0f6efd86df8c042459a
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2019-07-12 10:45:42 +0530

    Update q7.py

--- q7.py
+++ q7.py
@@ -1,11 +1,10 @@
-
 import matplotlib.pyplot as plt
 import math
 import numpy as np
 
 x=np.arange(0,10,0.1)
-y1=math.e-((x/10)*np.sin(math.pi*x))
-y2=math.e*x-(x/3)
+y1=np.exp(-(x/10))*np.sin(math.pi*x)
+y2=x*np.exp(-(x/3))
 plt.plot(x,y1,label='f(x)')
 plt.plot(x,y2,label='g(x)')
 plt.legend()
@@ -13,3 +12,5 @@ plt.xlabel('x-axis')
 plt.ylabel('y-axis')
 plt.savefig('graph.jpg')
 plt.show()
+
+
