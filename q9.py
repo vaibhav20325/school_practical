a50a63dd079982f24864f68134c97ffef5a96f83
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2019-07-24 11:02:14 +0530

    Create q9.py

---
+++ q9.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+array= np.random.randint(10, size=(5,5))
+print(array)
+print('Maximum value is: ',np.max(array))
+print('Minimum value is: ',np.min(array))
+
+scaler=int(input('Enter Number: '))
+new_array=array-scaler
+new_array=np.absolute(new_array)
+print(new_array)
+closest_diff=np.min(new_array)
+for i in range(5):
+    for j in range (5):
+        if new_array[i][j]==closest_diff:
+            print(array[i][j])
