a4f6ab0d877b5a0f1458efaa05eb727df9bcf511
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2019-07-29 22:44:25 +0530

    Delete seriesABCD.py

--- seriesABCD.py
+++
@@ -1,9 +0,0 @@
-
-cnt=0
-n=int(input("Enter number of rows: "))
-for i in range(0,n,1):
-    cnt=cnt + i
-    print((n-i-1)*" ", end="")
-    for j in range(cnt,i+1+cnt,1):
-        print(chr(j%26+ord("A")), end=" ")
-    print()
