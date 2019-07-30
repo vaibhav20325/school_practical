20758ee015e5071397297d58c9229cde1273dadd
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2019-07-30 23:23:53 +0530

    Update q9.py

--- q9.py
+++ q9.py
@@ -1,33 +1,15 @@
 import numpy as np
 
-array= np.random.randint(10, size=(5,5))
-print(array)
-print('Maximum value is: ',np.max(array))
-print('Minimum value is: ',np.min(array))
-
-scaler=int(input('Enter Number: '))
-new_array=array-scaler
-new_array=np.absolute(new_array)
-print(new_array)
-closest_diff=np.min(new_array)
-for i in range(5):
-    for j in range (5):
-        if new_array[i][j]==closest_diff:
-            print(array[i][j])
-
-            
-            
-import numpy as np
-
-array= np.random.randint(0,10,25)
+array= (np.random.random(25)*5).round(1)
 print(array.reshape((5,5)))
+max=0
+for i in array:
+    if list(array).count(i)> max:
+        max=i
+print("Most frequently occuring:", max)
 print('Maximum value is: ',np.max(array))
 print('Minimum value is: ',np.min(array))
 
 scaler=int(input('Enter Number: '))
-new_array=array-scaler
-new_array=np.absolute(new_array)
-print(new_array)
-
-closest=np.argmin(new_array)
-print(closest)
+closest=np.argmin(np.absolute(array-scaler))
+print('CLosest element is: ',array[closest])
