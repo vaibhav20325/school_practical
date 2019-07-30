f40a4d9357e971c856f27c6a512d4d10d079d506
Vaibhav <39609556+vaibhav20325@users.noreply.github.com>
2019-07-30 22:45:28 +0530

    Update q5.py

--- q5.py
+++ q5.py
@@ -1,12 +1,14 @@
 def base_convert(i,base):
-    if i<base:
-        if i>9:
+    if i < base:
+        if i > 9:
             return chr(ord('A')+i%10)
         else:
             return i
     else:
-        return str(base_convert(i//base,base))+str(i%base)
-    
+        if i % base<10:    
+            return str(base_convert(i//base,base))+str(i%base)
+        else:
+            return str(base_convert(i//base,base))+chr(ord('A')+(i%base)%10)
 num=int(input('Enter Number in Decimal: '))
 base=input('Desired Base (B for Binary, O for Octal, H for Hexadecimal): ')
 
@@ -17,4 +19,3 @@ except:
     base=int(base)
 
 print(base_convert(num,base))
-        
